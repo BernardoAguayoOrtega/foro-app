10c676f1bf6c6f866c4e58ed19c1f89b37b3bdfb
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-17 15:27:29 -0500

    Updates

---
+++ precache-manifest.c737c6dd4782a2c55394cf12673644ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "097f81adbd2f88a04268af3d6ca2900d",
+    "url": "/foro-app/index.html"
+  },
+  {
+    "revision": "df77e0acbb3d132144d6",
+    "url": "/foro-app/static/css/main.17d3d80c.chunk.css"
+  },
+  {
+    "revision": "e039bbf53d8541159757",
+    "url": "/foro-app/static/js/2.b74438e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foro-app/static/js/2.b74438e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df77e0acbb3d132144d6",
+    "url": "/foro-app/static/js/main.07530cb0.chunk.js"
+  },
+  {
+    "revision": "9765671c835217d75fe2",
+    "url": "/foro-app/static/js/runtime-main.f38799fb.js"
+  }
+]);
