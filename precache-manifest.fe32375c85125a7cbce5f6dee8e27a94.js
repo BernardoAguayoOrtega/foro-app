fe5a94f6ce9a5d340ca9712c4c2e1e5d2aa33765
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-09-24 11:55:51 -0500

    Updates

---
+++ precache-manifest.fe32375c85125a7cbce5f6dee8e27a94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a89dc5637abb8da63496ebdcb565b215",
+    "url": "/foro-app/index.html"
+  },
+  {
+    "revision": "77858d1b4cc539e8c5c7",
+    "url": "/foro-app/static/css/main.4d1db03f.chunk.css"
+  },
+  {
+    "revision": "a5522272746163cac3fd",
+    "url": "/foro-app/static/js/0.db8370ee.chunk.js"
+  },
+  {
+    "revision": "1aa0ec9baf1d467980ef",
+    "url": "/foro-app/static/js/3.6df2604c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foro-app/static/js/3.6df2604c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "793ac85692868d796b4d",
+    "url": "/foro-app/static/js/4.b91280dc.chunk.js"
+  },
+  {
+    "revision": "d0b76b5c81e68d5c5e03",
+    "url": "/foro-app/static/js/5.6d48e9eb.chunk.js"
+  },
+  {
+    "revision": "b6cf6c37b0b632bd9054",
+    "url": "/foro-app/static/js/6.eb32041e.chunk.js"
+  },
+  {
+    "revision": "1660494c63dff2e5a2ba",
+    "url": "/foro-app/static/js/7.d67a9b39.chunk.js"
+  },
+  {
+    "revision": "77858d1b4cc539e8c5c7",
+    "url": "/foro-app/static/js/main.045f9578.chunk.js"
+  },
+  {
+    "revision": "f9d801371633a66e350a",
+    "url": "/foro-app/static/js/runtime-main.53e36ca2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/foro-app/precache-manifest.4f9321f2aebe3c5c3bf108807945ba17.js"
+  "/foro-app/precache-manifest.fe32375c85125a7cbce5f6dee8e27a94.js"
 );
 
 self.addEventListener('message', (event) => {
